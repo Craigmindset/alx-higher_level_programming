f582c35b68c1a824190edc7f19ddb021e33fb6db
Craigmindset <irediacraig@gmail.com>
2023-07-25 15:44:00 -0700

    updated

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,20 @@
-#!/usr/bin/node
-const request = require('request');
-const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
-  }
-});
+!/usr/bin/node
+import requests
+
+def get_movie_title(episode_number):
+    url = "https://swapi-api.alx-tools.com/api/films/{}".format(episode_number)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data["title"]
+    else:
+        return None
+
+if __name__ == "__main__":
+    episode_number = int(input("Enter the episode number: "))
+    title = get_movie_title(episode_number)
+    if title:
+        print(title)
+    else:
+        print("No movie found for episode number {}.".format(episode_number))
+
